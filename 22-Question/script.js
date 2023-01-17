17785e6d26d7e034b97c0c91d4de1211211e9b9d
Pritam Chauhan <rajputpritam619@gmail.com>
2023-01-17 23:54:51 +0530

    Q21 and Q-22 done

---
+++ 22-Question/script.js
@@ -0,0 +1,26 @@
+// 22. The following is an array of 10 students ages:
+//     => const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+//     - Sort the array and find the min and max age
+//     - Find the median age(one middle item or two middle items divided by two)
+//     - Find the average age(all items divided by number of items)
+//     - Find the range of the ages(max minus min)
+//     - Compare the value of (min - average) and (max - average), use abs() method
+
+'use strict';
+
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+let max = 0;
+console.log(`The original array is`);
+console.log(ages);
+for(let i = 0; i<=ages.length-1; i++){
+ for(let j = 0; j<(ages.length-i-1); j++){
+    if(ages[j]>ages[j+1]){
+        let temp = ages[j];
+        ages[j] = ages[j+1];
+        ages[j+1] = temp;
+    }
+ }
+}
+console.log("The sorted array is")
+console.log(ages);
+console.log(`Min: ${ages[0]} and Max: ${ages[ages.length-1]}`)
